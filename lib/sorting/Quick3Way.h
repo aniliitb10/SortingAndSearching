148d272aa264f6bcdae459a547e934cfe2093f00
Anil Kumar <aniliitb10@gmail.com>
2025-08-28 00:00:17 +0530

    Tmp commit

--- lib/sorting/Quick3Way.h
+++ lib/sorting/Quick3Way.h
@@ -23,24 +23,25 @@ public:
 
 private:
   template <typename Itr, typename DiffType = ItrDiffType<Itr>>
-  static void sortImpl(const Itr& begin_, DiffType lo_, DiffType hi_)
+  static void sortImpl(const Itr& begin_, const DiffType lo_, const DiffType hi_)
   {
     if (lo_ >= hi_) return;
 
     auto lt = lo_;
     auto gt = hi_;
     auto index = lo_ + 1;
-    const auto v = at(begin_, lo_);
+    const auto pivot = at(begin_, lo_); // must be a value, not a reference
 
     while(index <= gt)
     {
-      const auto& ai = at(begin_, index);
+      const auto& v = at(begin_, index);
 
-      if      (ai < v) std::swap(at(begin_, index++), at(begin_, lt++));
-      else if (v < ai) std::swap(at(begin_, index), at(begin_, gt--));
-      else             index++;
+      if      (v < pivot) std::swap(at(begin_, index++), at(begin_, lt++));
+      else if (pivot < v) std::swap(at(begin_, index), at(begin_, gt--));
+      else                ++index;
     }
 
+    // is it that the values in [lt, gt] are same?
     sortImpl(begin_, lo_, lt-1);
     sortImpl(begin_, gt+1, hi_);
   }
--- lib/sorting/QuickSort.h
+++ lib/sorting/QuickSort.h
@@ -33,15 +33,26 @@ private:
   }
 
   template <typename Itr, typename DiffType = ItrDiffType<Itr>>
-  static DiffType partition(const Itr& begin_, DiffType lo_, DiffType hi_)
+  static DiffType partition(const Itr& begin_, const DiffType lo_, const DiffType hi_)
   {
+
+    // Some comments on the values of i and j
+    // i is initialized with lo_ but that value is never used in the while loop
+    // - while loop starts from ++i => hence, effectively, while loop runs with i = lo_+1
+    // - which is okay and expected, because the value at lo_ is used as the pivot
+    // Similarly, j is initialized with hi_+1 but while loop runs with --j
+    // - Hence, effectively, while loop runs with j = hi_
+    // So, with these non-intuitive initializations:
+    // - value at lo_ is used as pivot
+    // - while loop runs for [lo_+1, hi_]
+
     auto i = lo_;
     auto j = hi_ + 1;
     const auto v = at(begin_, lo_); // must not be referenced
 
     while(true)
     {
-      while(at(begin_, ++i) < v) // item at i is the
+      while(at(begin_, ++i) < v)
       {
         if (i == hi_) break;
       }
